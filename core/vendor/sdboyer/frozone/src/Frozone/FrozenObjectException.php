78e93ce4a0880175e5e983a5fae5d8a120492765
Luis Cordova <cordoval@gmail.com>
2014-05-25 22:46:16 -0500

    remove frozen library to easy review

--- core/vendor/sdboyer/frozone/src/Frozone/FrozenObjectException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Frozone;
-
-/**
- * Exception thrown when a write-protected operation is attempted on a frozen
- * object.
- */
-class FrozenObjectException extends ObjectStateException {}
--- core/vendor/sdboyer/frozone/src/Frozone/ObjectStateException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Frozone;
-
-/**
- * Parent to exceptions thrown when a write-protected operation is attempted
- * on a frozen object.
- */
-abstract class ObjectStateException extends \LogicException {}
